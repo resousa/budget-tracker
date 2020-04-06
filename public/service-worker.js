321913ee97eb33a5bce8305fb1bf9dfd38949ea6
Ryan Sousa <Sousa.Ryan4@gmail.com>
2020-04-05 21:17:20 -0700

    update service worker

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,3 +1,74 @@
-'use strict';
+const FILES_TO_CACHE = [
+  '/',
+  '/index.html',
+  '/db.js',
+  '/index.js',
+  '/manifest.webmanifest',
+  '/assets/images/calculator-icon-sm.png',
+  '/assets/images/calculator-icon.png',
+  'style.css'
+];
 
-console.log(`Hello from your service worker!`);
+const CACHE_NAME = 'static-cache-v2';
+const DATA_CACHE_NAME = 'data-cache-v1';
+
+self.addEventListener(`install`, event => {
+  console.log(`begin install`);
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      console.log(`Your files were pre-cached successfully!`);
+      return cache.addAll(FILES_TO_CACHE);
+    })
+  );
+
+  self.skipWaiting();
+});
+
+self.addEventListener(`activate`, event => {
+  console.log(`being activate`);
+  event.waitUntil(
+    caches.keys().then(keyList =>
+      Promise.all(
+        keyList.map(key => {
+          if (key !== CACHE_NAME && key !== DATA_CACHE_NAME) {
+            console.log(`Removing old cache data`, key);
+            return caches.delete(key);
+          }
+          return undefined;
+        })
+      )
+    )
+  );
+
+  self.clients.claim();
+});
+
+self.addEventListener(`fetch`, event => {
+  console.log(`begin fetch`);
+  if (event.request.url.includes(`/api/`)) {
+    event.respondWith(
+      caches
+        .open(DATA_CACHE_NAME)
+        .then(cache =>
+          fetch(event.request)
+            .then(response => {
+              if (response.status === 200) {
+                cache.put(event.request.url, response.clone());
+              }
+
+              return response;
+            })
+            .catch(() => cache.match(event.request))
+        )
+        .catch(err => console.error(err))
+    );
+  } else {
+    event.respondWith(
+      caches
+        .match(event.request)
+        .then(response => response || fetch(event.request))
+    );
+  }
+});
+
+console.log('Hi from your service-worker.js file!');
